e41e0d6b35c6ce37f2829946f6d8823519d192fc
Ezequiel Romero <ezequielromero3039@gmail.com>
2023-04-17 14:10:04 -0300

    Agregando archivo index, script, style y la imagen

---
+++ romero_carlos_ezequiel/script.js
@@ -0,0 +1,14 @@
+
+logo.onclick = function () {
+    let start = Date.now();
+
+    let timer = setInterval(function () {
+        let timePassed = Date.now() - start;
+
+        logo.style.left = timePassed / 2 + 'px';
+
+        if (timePassed > 2000) clearInterval(timer);
+
+    }, 20);
+}
+
